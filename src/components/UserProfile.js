d932ff47098f102c77f1c750d34821b2298b2ad1
Zachary Soliman <zachsoliman1@gmail.com>
2022-08-01 11:41:21 -0400

    fetch request set up

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,7 +1,23 @@
 import UserInfo from './UserInfo'
 import UserPics from './UserPics'
+import {useState, useEffect} from 'react'
+
 
 const UserProfile = () => {
+    const [userData, setUserData] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+          let req = await fetch('http://localhost:3001/users')
+          let res = await req.json()
+          setUserData(res)
+
+        }
+        getData()
+    },[])
+
+console.log(userData)
+
     return (
       <div>
           <UserInfo />
