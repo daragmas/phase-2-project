d11ae69478b6bb94634629a19689975818a080fe
Zachary Soliman <zachsoliman1@gmail.com>
2022-08-02 19:17:25 -0400

    home styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,30 @@
 import UserProfile from "./UserProfile"
+import {useState, useEffect} from 'react'
 
 const Home = ({curUser})=>{
-
     // console.log("Home curUser", curUser)
 
+    const [homeImage, setHomeImage] = useState([])
+    useEffect(() => {
+      const grabImages = async () => {
+        let req = await fetch('http://localhost:3001/images')
+        let res = await req.json()
+        setHomeImage(res)
+      }
+      grabImages()
+    }, [])
+
+  return(
+
+        <div className='homeImageDiv'>
+            <h1 style={{ color: 'white', fontVariant: 'small-caps', padding: 20 }}>Welcome to Albm!</h1>
+
+            {homeImage.map((image) => {
+              return(
+              <img className='homeImage' src={image.source} key={image.id}/>
+            )})}
 
 
-
-
-    return(
-        <div>
-            <h1>check out this awesome homepage</h1>
         </div>
 
 
