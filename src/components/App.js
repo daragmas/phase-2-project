0c0ad0d5297da32c5ee60a53e63e8aa7650717d3
daragmas <daragmas@gmail.com>
2022-08-02 16:45:40 -0400

    testing stuff

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,7 @@ const App = () => {
     console.log('UserChange')
   }
 
+  
 
   return (
     <div>
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,18 +2,6 @@ import { useState, useEffect } from 'react'
 import FavoritesButton from './FavoritesButton'
 
 const ImageCard = ({ image }) => {
-  // const [userImages, setUserImages] = useState([])
-
-  // useEffect(() => {
-  //   const getImages = async () => {
-  //     let req = await fetch('http://localhost:3001/images')
-  //     let res = await req.json()
-  //     setUserImages(res.filter((image) => image.imageOwner == userId ? image : null))
-  //   }
-  //   getImages()
-  // }, [])
-
-
   return (
     <div className='divImageCard'>
 
